0f56b7428ca7ee80cbd4ac791dccc43befdea5a6
Gabriel Clemente <clementeaquipresente@yahoo.com>
2022-07-15 16:35:35 +0200

    Web Vitals Report Set Up

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -54,5 +54,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
