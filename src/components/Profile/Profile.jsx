b5da7179f1e59c94f61acf8fecaab0fbc73384e4
Gabriel Clemente <clementeaquipresente@yahoo.com>
2022-07-21 10:11:58 +0200

    basic modeling done

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,10 +7,9 @@ export default function Profile() {
   return (
     <>
       <Main pad="large">
-        <Heading>Hola Roberto!</Heading>
+        <Heading>Hola Roberta!</Heading>
         <Box direction="row"  gap="small">
           <Avatar src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80" />   
-          <Avatar background="accent-2">
             <User color="accent-1" />
           </Avatar>
         </Box>
