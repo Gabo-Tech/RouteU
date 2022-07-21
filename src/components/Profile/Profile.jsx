7f39a9f1a6dc10c82dcab7dea37d2002392e2ed7
Gabriel Clemente <clementeaquipresente@yahoo.com>
2022-07-21 15:06:48 +0200

    form local states almost done

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -8,10 +8,12 @@ export default function Profile() {
     <>
       <Main pad="xlarge">
         <Heading>Hola Roberta!</Heading>
-        <Box direction="row"  gap="small">
-          <Avatar src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80" />   
+        <Box direction="row" justify="center" gap="small" pad="large">
+          <Avatar size="xlarge" src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80" />   
         </Box>
-        <Text>Tus rutas favoritas</Text>
+        <Box direction="row" justify="center" gap="small">
+          <Text direction="row" justify="center">Tus rutas favoritas</Text>
+        </Box>
         <InfiniteScroll favouriteRoutes={[1, 2, 3, 4, 5, 6, 7]}>
           {(item) => (
             <Box
