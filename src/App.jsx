b135871f3964b6c62e5e4dc47e881a45a1c7c647
Gabriel Clemente <clementeaquipresente@yahoo.com>
2022-07-21 02:31:05 +0200

    basic modeling done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import AppBar from './components/AppBar/AppBar';
-// import Navbar from './components/Navbar/Navbar';
-// import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
 import MyForm from './components/Form/Form';
 import Profile from './components/Profile/Profile';
 import MyRoutes from './components/Routes/Routes';
-// import PostDetails from './components/PostDetails/PostDetails';
-// import CreatorOrTag from './components/CreatorOrTag/CreatorOrTag';
-// import Footer from './components/Footer/Footer';
-// import NotFound from './components/NotFound/NotFound';
-// import PrivateZone from './components/Guards/PrivateZone';
+
 export default function App() {
   // const user = JSON.parse(localStorage.getItem('profile'));
   return (
