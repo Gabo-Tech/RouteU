a4b707e0c8739ac6d48eb7a1890297005560f007
Gabriel Clemente <clementeaquipresente@yahoo.com>
2022-07-21 14:56:31 +0200

    form local states almost done

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,18 +1,25 @@
 //Registro y inicio de sesión
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 // import styles from './Auth.module.css';
 import { Box, Heading, TextInput, FormField, Button, Menu , Main/*Heading, Grommet, grommet*/ } from 'grommet';
 
-const initialState = { age: 'Edad', gender: 'Género', company: 'Acompañamiento', duration: '¿De cuánto tiempo dispones para realizar la ruta?', price: 'Coste dispuesto a pagar', difficulty: 'Dificultad', transport: 'Transporte', type: 'Tipo de ruta preferida', favouriteRoutes:[] };
-
 export default function MyForm() {
+  const [favRoutes, setFavRoutes] = useState([]);
+  const initialState = { age: 'Edad', gender: 'Género', company: 'Acompañamiento', duration: '¿De cuánto tiempo dispones para realizar la ruta?', price: 'Coste dispuesto a pagar', difficulty: 'Dificultad', transport: 'Transporte', type: 'Tipo de ruta preferida', favouriteRoutes:favRoutes, sendBtn:'Enviar' };
 	const [form, setForm] = useState(initialState);
-	const [isSignup, setIsSignup] = useState(false);
 	const switchMode = () => {
-        setForm(initialState);
-        setIsSignup((prevIsSignup) => !prevIsSignup);
+        // setTimeout(setForm(initialState), 5000);
+        console.log(JSON.stringify(favRoutes));
+        setForm({...form, sendBtn: '¡Enviado!'});
+        // setForm({ ...form, favouriteRoutes:favRoutes });
+        console.log(JSON.stringify(form));
     };
-	const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+  const selectSeveralRoutes= (e,routeId)=>{
+    e.preventDefault();
+    e.target.style.backgroundColor = 'blue';
+    setFavRoutes([...favRoutes, routeId])
+    setForm({ ...form, favouriteRoutes:favRoutes });
+  }
   return (
     <>
         <Main id="Main" fill="vertical"  pad="xlarge">
@@ -28,92 +35,93 @@ export default function MyForm() {
             ]}
           />
           <Menu
-            label="Género"
+            label={form?.gender}
             items={[
-              { label: 'Mujer', onClick: () => {} },
-              { label: 'Hombre', onClick: () => {} },
-              { label: 'Prefiero no decirlo', onClick: () => {} }
+              { label: 'Mujer', onClick: () => {setForm({...form, gender: 'Mujer'}) } },
+              { label: 'Hombre', onClick: () => {setForm({...form, gender: 'Hombre'}) } },
+              { label: 'Prefiero no decirlo', onClick: () => {setForm({...form, gender: 'Prefiero no decirlo'}) } }
             ]}
           />
           <Menu
             label={form?.company}
             items={[
-              { label: 'Sólo', onClick: () => {} },
-              { label: 'Pareja', onClick: () => {} },
-              { label: 'Familia', onClick: () => {} }
+              { label: 'Sólo', onClick: () => {setForm({...form, company: 'Sólo'}) } },
+              { label: 'Pareja', onClick: () => {setForm({...form, company: 'Pareja'}) } },
+              { label: 'Familia', onClick: () => {setForm({...form, company: 'Familia'}) } }
             ]}
           />
           <Menu
             label={form?.duration}
             items={[
-              { label: '1 - 2 horas', onClick: () => {} },
-              { label: '3 - 4 horas', onClick: () => {} },
-              { label: '5 - 6 horas', onClick: () => {} },
-              { label: '7 - 8 horas', onClick: () => {} },
+              { label: '1 - 2 horas', onClick: () => {setForm({...form, duration: '1 - 2 horas'})} },
+              { label: '3 - 4 horas', onClick: () => {setForm({...form, duration: '3 - 4 horas'})} },
+              { label: '5 - 6 horas', onClick: () => {setForm({...form, duration: '5 - 6 horas'})} },
+              { label: '7 - 8 horas', onClick: () => {setForm({...form, duration: '7 - 8 horas'})} },
             ]}
           />
           <Menu
             label={form?.price}
             items={[
-              { label: 'First Action', onClick: () => {} },
-              { label: '1 - 50€', onClick: () => {} },
-              { label: '+50€', onClick: () => {} }
+              { label: 'Free', onClick: () => {setForm({...form, price: 'Free'})} },
+              { label: '1 - 50€', onClick: () => {setForm({...form, price: '1 - 50€'})} },
+              { label: '+50€', onClick: () => {setForm({...form, price: '+50€'})} }
             ]}
           />
           <Menu
             label={form?.difficulty}
             items={[
-              { label: 'Alta', onClick: () => {} },
-              { label: 'Baja', onClick: () => {} }
+              { label: 'Alta', onClick: () => {setForm({...form, difficulty: 'Alta'})} },
+              { label: 'Baja', onClick: () => {setForm({...form, difficulty: 'Baja'})} }
             ]}
           />
           <Menu
             label={form?.transport}
             items={[
-              { label: 'Pie', onClick: () => {} },
-              { label: 'Bicicleta', onClick: () => {} }
+              { label: 'A Pie', onClick: () => {setForm({...form, transport: 'A Pie'})} },
+              { label: 'En Bicicleta', onClick: () => {setForm({...form, transport: 'En Bicicleta'})} }
             ]}
           />
           <Menu
             label={form?.type}
             items={[
-              { label: 'Histórica', onClick: () => {} },
-              { label: 'Turística', onClick: () => {} },
-              { label: 'Literaria', onClick: () => {} },
-              { label: 'Patrimonio', onClick: () => {} },
+              { label: 'Histórica', onClick: () => {setForm({...form, type: 'Histórica'})} },
+              { label: 'Turística', onClick: () => {setForm({...form, type: 'Turística'})} },
+              { label: 'Literaria', onClick: () => {setForm({...form, type: 'Literaria'})} },
+              { label: 'Patrimonio', onClick: () => {setForm({...form, type: 'Patrimonio'})} },
             ]}
           />
           <Menu
             label="Selecciona tu/s rutas favorita/s"
             items={[
-              { label: 'Arbres monumentals i singulars', onClick: () => {} },
-              { label: 'Arbres: La volta al món', onClick: () => {} },
-              { label: 'Arbres: Els indians', onClick: () => {} },
-              { label: 'Arbres: Un passeig per l’Albereda', onClick: () => {} },
-              { label: 'Arbres: Un viatge pel Mediterrani', onClick: () => {} },
-              { label: 'Ruta Carrer Russafa - la Llotja', onClick: () => {} },
-              { label: 'Contes Blasco Ibañez – Ruta 1', onClick: () => {} },
-              { label: 'Contes Blasco Ibañez – Ruta 2', onClick: () => {} },
-              { label: 'Entre Naranjos', onClick: () => {} },
-              { label: 'Glorieta-Santa Catalina', onClick: () => {} },
-              { label: 'Jardins del Reial - Glorieta', onClick: () => {} },
-              { label: 'La barraca', onClick: () => {} },
-              { label: 'Ruta Cultural Anell Ciclista', onClick: () => {} },
-              { label: 'Ruta de la Seda', onClick: () => {} },
-              { label: "Ruta dels Pecats a l'entorn del Mercat Central", onClick: () => {} },
-              { label: 'Ruta pel Centre Històric', onClick: () => {} },
-              { label: "Ruta recinte enmurallat d'època islàmica", onClick: () => {} },
-              { label: 'Ruta València en bicicleta', onClick: () => {} },
-              { label: 'Sant Agustí – Pont de Fusta', onClick: () => {} },
-              { label: 'Sant Agustí - Portal Nou', onClick: () => {} },
-              { label: 'Serrans - Palau de la Generalitat', onClick: () => {} },
-              { label: 'Torres de Quart - Temple', onClick: () => {} },
-              { label: "Trinitat - L'Almodí", onClick: () => {} },
-              { label: 'Ruta Valencia en la Memòria', onClick: () => {} },
+              { label: 'Arbres monumentals i singulars', onClick: (e) => {selectSeveralRoutes(e,1)} },
+              { label: 'Arbres: La volta al món', onClick: (e) => {selectSeveralRoutes(e,2)} },
+              { label: 'Arbres: Els indians', onClick: (e) => {selectSeveralRoutes(e,3)} },
+              { label: 'Arbres: Un passeig per l’Albereda', onClick: (e) => {selectSeveralRoutes(e,4)} },
+              { label: 'Arbres: Un viatge pel Mediterrani', onClick: (e) => {selectSeveralRoutes(e,5)} },
+              { label: 'Ruta Carrer Russafa - la Llotja', onClick: (e) => {selectSeveralRoutes(e,6)} },
+              { label: 'Contes Blasco Ibañez – Ruta 1', onClick: (e) => {selectSeveralRoutes(e,7)} },
+              { label: 'Contes Blasco Ibañez – Ruta 2', onClick: (e) => {selectSeveralRoutes(e,8)} },
+              { label: 'Entre Naranjos', onClick: (e) => {selectSeveralRoutes(e,9)} },
+              { label: 'Glorieta-Santa Catalina', onClick: (e) => {selectSeveralRoutes(e,10)} },
+              { label: 'Jardins del Reial - Glorieta', onClick: (e) => {selectSeveralRoutes(e,11)} },
+              { label: 'La barraca', onClick: (e) => {selectSeveralRoutes(e,12)} },
+              { label: 'Ruta Cultural Anell Ciclista', onClick: (e) => {selectSeveralRoutes(e,13)} },
+              { label: 'Ruta de la Seda', onClick: (e) => {selectSeveralRoutes(e,14)} },
+              { label: "Ruta dels Pecats a l'entorn del Mercat Central", onClick: (e) => {selectSeveralRoutes(e,15)} },
+              { label: 'Ruta pel Centre Històric', onClick: (e) => {selectSeveralRoutes(e,16)} },
+              { label: "Ruta recinte enmurallat d'època islàmica", onClick: (e) => {selectSeveralRoutes(e,17)} },
+              { label: 'Ruta València en bicicleta', onClick: (e) => {selectSeveralRoutes(e,18)} },
+              { label: 'Sant Agustí – Pont de Fusta', onClick: (e) => {selectSeveralRoutes(e,19)} },
+              { label: 'Sant Agustí - Portal Nou', onClick: (e) => {selectSeveralRoutes(e,20)} },
+              { label: 'Serrans - Palau de la Generalitat', onClick: (e) => {selectSeveralRoutes(e,21)} },
+              { label: 'Torres de Quart - Temple', onClick: (e) => {selectSeveralRoutes(e,22)} },
+              { label: "Trinitat - L'Almodí", onClick: (e) => {selectSeveralRoutes(e,23)} },
+              { label: 'Ruta Valencia en la Memòria', onClick: (e) => {selectSeveralRoutes(e,24)} },
             ]}
           />
           
           </Box>
+          <Button label={form?.sendBtn} onClick={switchMode} pad="medium" margin={{vertical: 'large', horizontal: 'xsmall' }}/>
         </Main>
     </>
   )
