3227de3e4dc6fb43bc10e0199bba455e0044c62d
Gabriel Clemente <clementeaquipresente@yahoo.com>
2022-07-25 00:08:48 +0200

    Rating bug disminuido

--- src/components/Feed/Routes/Route/Route.jsx
+++ src/components/Feed/Routes/Route/Route.jsx
@@ -8,48 +8,142 @@ import { like, dislike, rate } from "./../../../../features/routes/routesSlice"
 import { HeartOutlined, HeartFilled } from "@ant-design/icons";
 import { relativeTimeRounding } from 'moment';
 import axios from 'axios';
+import { useTimeout } from '../../../../hooks/useTimeout';
 const Route = () => {  
-  const { user } = useSelector(state => state.auth);
-  const { routes } = useSelector((state) => state.routes);
-  // const { avgRate} = useSelector((state) => state.avgRate);
-  // console.log(avgRate)
-  const dispatch = useDispatch();
-
-
-    const [currentValue, setCurrentValue] = useState(2.5)
-    const [rates, setRates] = useState(2.5)
+    const { user } = useSelector(state => state.auth);
+    const { routes } = useSelector((state) => state.routes);
+    // const { avgRate} = useSelector((state) => state.avgRate);
+    // console.log(avgRate)
+    const dispatch = useDispatch();
+    // function roundS(value, step) {
+    //   step || (step = 1.0);
+    //   var inv = 1.0 / step;
+    //   return Math.round(value * inv) / inv;
+    // }
+    const [currentValue, setCurrentValue] = useState('.  Califica la ruta para ver la media')
+    const [rates, setRates] = useState(0)
   
-  
-
+    let elementId = "";
+    const getElementOnClick = event => {
+         elementId=event.currentTarget.id;
+    };
+    function showValue(value){
+              setRates(value);
+            }
+    async function rating(value,routeIdL){
+        if(elementId=== routeIdL){
+            dispatch(rate(routeIdL));
+            await axios.post(`http://localhost:8080/ratings/${routeIdL}`,{rating:value},{headers: {authorization: user?.token,}});
+            const resVal = await axios.get(`http://localhost:8080/ratings/`,{headers: {authorization: user?.token,}});
+            const array = resVal.data.ratings;
+            const filteredArray = array.filter(element => element.routeId === routeIdL && element.rating !== null && element.rating !== undefined);
+            const rateTotal = filteredArray.reduce(function(prev, cur) {
+              return prev + cur.rating;
+            }, 0);
+            const averageRate=rateTotal/filteredArray.length
+            const normalValue= parseFloat(averageRate).toFixed(2);
+            const averageRating = document.getElementById(elementId+"p");
+            averageRating.innerText="Puntuación media de la ruta "+normalValue;
+            // const averageRatingStars = document.getElementById(elementId);
+            // averageRatingStars.innerHTML=`<ul class="ant-rate" tabindex="0" role="radiogroup">
+            //                                 <li class="ant-rate-star ant-rate-star-full">
+            //                                   <div role="radio" aria-checked="${normalValue>0?true:false}" aria-posinset="1" aria-setsize="5" tabindex="0">
+            //                                     <div class="ant-rate-star-first">
+            //                                       <span role="img" aria-label="star" class="anticon anticon-star">
+            //                                         <svg viewBox="64 64 896 896" focusable="false" data-icon="star" width="1em" height="1em" fill="currentColor" aria-hidden="true">
+            //                                           <path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 00.6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0046.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"></path>
+            //                                         </svg>
+            //                                       </span>
+            //                                     </div>
+            //                                     <div class="ant-rate-star-second">
+            //                                       <span role="img" aria-label="star" class="anticon anticon-star">
+            //                                         <svg viewBox="64 64 896 896" focusable="false" data-icon="star" width="1em" height="1em" fill="currentColor" aria-hidden="true">
+            //                                           <path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 00.6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0046.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"></path>
+            //                                         </svg>
+            //                                       </span>
+            //                                     </div>
+            //                                   </div>
+            //                                 </li>
+            //                                 <li class="ant-rate-star ant-rate-star-full">
+            //                                   <div role="radio" aria-checked="${normalValue>=2?true:false}" aria-posinset="2" aria-setsize="5" tabindex="0">
+            //                                     <div class="ant-rate-star-first">
+            //                                       <span role="img" aria-label="star" class="anticon anticon-star">
+            //                                         <svg viewBox="64 64 896 896" focusable="false" data-icon="star" width="1em" height="1em" fill="currentColor" aria-hidden="true">
+            //                                           <path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 00.6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0046.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"></path>
+            //                                         </svg>
+            //                                       </span>
+            //                                     </div>
+            //                                     <div class="ant-rate-star-second">
+            //                                       <span role="img" aria-label="star" class="anticon anticon-star">
+            //                                         <svg viewBox="64 64 896 896" focusable="false" data-icon="star" width="1em" height="1em" fill="currentColor" aria-hidden="true">
+            //                                           <path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 00.6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0046.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"></path>
+            //                                         </svg>
+            //                                       </span>
+            //                                     </div>
+            //                                   </div>
+            //                                 </li>
+            //                                 <li class="ant-rate-star ant-rate-star-full">
+            //                                   <div role="radio" aria-checked="${normalValue>=3?true:false}" aria-posinset="3" aria-setsize="5" tabindex="0">
+            //                                     <div class="ant-rate-star-first">
+            //                                       <span role="img" aria-label="star" class="anticon anticon-star">
+            //                                         <svg viewBox="64 64 896 896" focusable="false" data-icon="star" width="1em" height="1em" fill="currentColor" aria-hidden="true">
+            //                                           <path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 00.6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0046.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"></path>
+            //                                         </svg>
+            //                                       </span>
+            //                                     </div>
+            //                                     <div class="ant-rate-star-second">
+            //                                       <span role="img" aria-label="star" class="anticon anticon-star">
+            //                                         <svg viewBox="64 64 896 896" focusable="false" data-icon="star" width="1em" height="1em" fill="currentColor" aria-hidden="true">
+            //                                           <path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 00.6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0046.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"></path>
+            //                                         </svg>
+            //                                       </span>
+            //                                     </div>
+            //                                   </div>
+            //                                 </li>
+            //                                 <li class="ant-rate-star ant-rate-star-zero">
+            //                                   <div role="radio" aria-checked="${normalValue>=4?true:false}" aria-posinset="4" aria-setsize="5" tabindex="0">
+            //                                     <div class="ant-rate-star-first">
+            //                                       <span role="img" aria-label="star" class="anticon anticon-star">
+            //                                         <svg viewBox="64 64 896 896" focusable="false" data-icon="star" width="1em" height="1em" fill="currentColor" aria-hidden="true">
+            //                                           <path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 00.6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0046.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"></path>
+            //                                         </svg>
+            //                                       </span>
+            //                                     </div>
+            //                                     <div class="ant-rate-star-second">
+            //                                       <span role="img" aria-label="star" class="anticon anticon-star">
+            //                                         <svg viewBox="64 64 896 896" focusable="false" data-icon="star" width="1em" height="1em" fill="currentColor" aria-hidden="true">
+            //                                           <path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 00.6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0046.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"></path>
+            //                                         </svg>
+            //                                       </span>
+            //                                     </div>
+            //                                   </div>
+            //                                 </li>
+            //                                 <li class="ant-rate-star ant-rate-star-zero">
+            //                                   <div role="radio" aria-checked="${normalValue===5?true:false}" aria-posinset="5" aria-setsize="5" tabindex="0">
+            //                                     <div class="ant-rate-star-first">
+            //                                       <span role="img" aria-label="star" class="anticon anticon-star">
+            //                                         <svg viewBox="64 64 896 896" focusable="false" data-icon="star" width="1em" height="1em" fill="currentColor" aria-hidden="true">
+            //                                           <path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 00.6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0046.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"></path>
+            //                                         </svg>
+            //                                       </span>
+            //                                     </div>
+            //                                     <div class="ant-rate-star-second">
+            //                                       <span role="img" aria-label="star" class="anticon anticon-star">
+            //                                         <svg viewBox="64 64 896 896" focusable="false" data-icon="star" width="1em" height="1em" fill="currentColor" aria-hidden="true">
+            //                                           <path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 00.6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0046.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"></path>
+            //                                         </svg>
+            //                                       </span>
+            //                                     </div>
+            //                                   </div>
+            //                                 </li>
+            //                               </ul>`
+            setRates(normalValue);
+        }
+    }
   
   
     const routeList = routes?.map((elements) => {
-    const isAlreadyLiked = elements.likes?.includes(user?.user._id); 
-    async function rating(value,routeIdL){
-      // const routeComponentId= document.getElementById(value);
-      // console.log("THIS IS THE ID OF THE CLICKED COMPONENT", routeComponentId);
-      // if(elements._id=== routeIdL){
-        dispatch(rate(routeIdL));
-        const res = await axios.post(`http://localhost:8080/ratings/${routeIdL}`,{rating:value},{headers: {authorization: user?.token,}});
-        console.log("THIS IS THE RATE CALL:",res);
-        const resVal = await axios.get(`http://localhost:8080/ratings/`,{headers: {authorization: user?.token,}});
-        console.log("THIS IS THE GETRATE CALL:",resVal);
-        const array = resVal.data.ratings;
-        console.log("This is array",array);
-        const filteredArray = array.filter(element => element.routeId === routeIdL && element.rating !== null && element.rating !== undefined);
-        console.log("This is filteredArray",filteredArray);
-        const rateTotal = filteredArray.reduce(function(prev, cur) {
-          return prev + cur.rating;
-        }, 0);
-        const averageRate=rateTotal/filteredArray.length
-        setCurrentValue(averageRate);
-        const normalValue= parseFloat(averageRate).toFixed(2);
-        setCurrentValue(normalValue);
-        setRates(value);
-
-      // }
-    }
-  
+    const isAlreadyLiked = elements.likes?.includes(user?.user._id);   
     return (
       <>
         <section id="mobileFirst" className="py-5">
@@ -93,10 +187,12 @@ const Route = () => {
                 )}
                 <span>{elements.likes?.length}</span>
               </div>
-                <Rate id={elements._id} allowHalf defaultValue={2.5} onChange={(value) => {rating(value, elements._id)}} value={rates}/>
+              <div id={elements._id} onClick={getElementOnClick}>
+                <Rate allowHalf defaultValue={2.5}  onFocus={(value) => {showValue(value)}} onChange={(value) => {rating(value, elements._id)}} value={rates}/>
+              </div>
               
                 </div>
-                <p> Puntuación media de la ruta {currentValue}</p>
+                <p id={elements._id+"p"} onClick={getElementOnClick}> Puntuación media de la ruta {currentValue}</p>
               </div>
             </div>
           </div>
