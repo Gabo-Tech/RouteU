929b4908db9413fe358a5064247fe2dea39b46d5
Gabriel Clemente <clementeaquipresente@yahoo.com>
2022-07-25 00:29:49 +0200

    checking that merge was succesful

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -56,7 +56,7 @@ if (userFavouriteRoutes.length <= 0){
       <Main className={styles.full} pad="xlarge">
         <Heading>Hola {username}!</Heading>
         <Box className={styles.pic} direction="row" justify="center" gap="small" pad="large">
-          <Avatar size="xlarge" src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80" />   
+          <Avatar size="xlarge" src="https://placeimg.com/380/230/arch" />   
         </Box>
         <Box direction="row" justify="center" gap="small">
           <Text direction="row" justify="center">Tus rutas favoritas</Text>
